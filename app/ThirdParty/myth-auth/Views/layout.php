5301ea4fc601cd540132b224d07236d8cd68579a
Micko Widi <widimicko85@gmail.com>
2021-12-22 21:36:28 +0700

    change auth lib to offline

--- app/ThirdParty/myth-auth/Views/layout.php
+++ app/ThirdParty/myth-auth/Views/layout.php
@@ -11,7 +11,7 @@
     <title>Aplikasi Absensi MI PSM AL AMIN SUMBERAGUNG</title>
 
     <!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="<?= base_url() ?>/library/auth/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             padding-top: 5rem;
@@ -33,9 +33,9 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="<?= base_url() ?>/library/auth/jquery.slim.min.js"></script>
+<script src="<?= base_url() ?>/library/auth/popper.min.js"></script>
+<script src="<?= base_url() ?>/library/auth/bootstrap.min.js"></script>
 
 <?= $this->renderSection('pageScripts') ?>
 </body>
